e37e089e66e7ba4bc5f4d35f9b5275dc2d12a664
sebastien-violante <sebastien.violante@gmail.com>
2023-11-23 22:10:15 +0100

    add new pages

---
+++ classesScripts.js
@@ -0,0 +1,25 @@
+class Animal {
+    constructor(age, color) {
+        this.age = age;
+        this.color = color;
+    }
+
+    displayInfo() {
+        console.log(`Your animal is ${this.age} and it's color is ${this.color}`);
+    }
+}
+
+const Poupoune = new Animal(2,'grey');
+
+Poupoune.displayInfo();
+
+const myFunction = (a,b) => a+b;
+
+console.log(myFunction(5,7));
+
+let divide = (a,b) => {
+    if(b === 0) { return 0 };
+    return a/b; }
+
+console.log(divide(2,3));
+console.log(divide(2,0));
