faef56364d292d6054a533cb0bd7f220f00b79cb
sebastien-violante <sebastien.violante@gmail.com>
2023-11-28 21:42:39 +0100

    beginjs exercice

--- beginjs.js
+++ beginjs.js
@@ -1,2 +1,32 @@
-/* generate a random number between 0 et 100 */
+const getRandomNumber = (min, max) => {
+    return(Math.floor(Math.random() * (max - min + 1)) + min );
+}
 
+const isValidNumber = (number) => {
+    return !Number.isNaN(number) && number >= 0 && number <= 100;
+}
+
+const game = () => {
+    const targetNumber = getRandomNumber(0,100);
+    let attemptCount = 0;
+
+    const play = () => {
+        let userGuess = Number(prompt('guess which number: '));
+        attemptCount++;
+        if(!isValidNumber(userGuess)) {
+            console.log("Your number is not in the right range");
+            return play();
+        }
+        if(userGuess > targetNumber) {
+            console.log('too high. Try again...');
+            return play();
+        }
+        if(userGuess < targetNumber) {
+            console.log('too low. Try again...');
+            return play();
+        }
+        console.log(`Well done. You found ${targetNumber} in only ${attemptCount} attemps !`)
+    }
+}
+
+game();
